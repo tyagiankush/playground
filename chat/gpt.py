1c1541d9583a9599c1adf548fd087fe917227945
ankush <tyagi.ankush94@gmail.com>
2025-02-02 19:10:35 +0000

    added ollama deepseek r1 rag document analyser

---
+++ chat/gpt.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+from chat.settings import settings
+
+
+def chat_with_llm(query: str) -> None:
+	client = OpenAI(api_key=settings.api_key)
+
+	completion = client.chat.completions.create(
+		model=settings.model_name, store=True, messages=[{'role': 'user', 'content': query}]
+	)
+
+	print(completion.choices[0].message)
+
+
+if __name__ == '__main__':
+	chat_with_llm('write a haiku about ai')
